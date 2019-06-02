12c1be2274f8f3bdfa8ce7adafd6f83a124b39d0
Suprianto D <ersaka96@gmail.com>
2019-06-02 14:45:24 +0800

    change api to online, release production 1

--- app/src/main/java/com/ertohru/pendonor/utils/ApiEndPoint.kt
+++ app/src/main/java/com/ertohru/pendonor/utils/ApiEndPoint.kt
@@ -3,8 +3,8 @@ package com.ertohru.pendonor.utils
 class ApiEndPoint{
 
     companion object {
-        const val PENDONOR = "http://192.168.56.1:8000/api/"
-        const val PENDONOR_UPLOADS = "http://192.168.56.1:8000/uploads/"
+        const val PENDONOR = "https://erthru.pro/pendonor/public/api/"
+        const val PENDONOR_UPLOADS = "https://erthru.pro/pendonor/public/uploads/"
     }
 
 }
